8eb2be0f58f3d9a86991644603d4063056c1c174
Anand Shastri <anandshastri23@gmail.com>
2017-11-11 17:06:30 -0600

    Add initial project

---
+++ src/main/java/server/test/peppermintTests.java
@@ -0,0 +1,16 @@
+package server.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class peppermintTests {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
